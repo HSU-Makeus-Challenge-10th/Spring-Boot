eda2f63b4521de465b68ee4841b26a10be8d1cd1
junho <2171168@hansung.ac.kr>
2026-04-02 01:10:52 +0900

    test

---
+++ junho/src/main/java/com/umc/junho/JunhoApplication.java
@@ -0,0 +1,13 @@
+package com.umc.junho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunhoApplication.class, args);
+    }
+}
+
