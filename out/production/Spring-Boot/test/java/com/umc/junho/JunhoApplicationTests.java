1912ae848a46046682a87ccd42ed7f4a49007173
junho <2171168@hansung.ac.kr>
2026-04-02 01:13:13 +0900

    teste

---
+++ out/production/Spring-Boot/test/java/com/umc/junho/JunhoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.umc.junho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
