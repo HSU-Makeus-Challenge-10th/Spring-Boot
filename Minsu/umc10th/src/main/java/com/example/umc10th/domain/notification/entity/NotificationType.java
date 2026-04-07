6a2ce27d5f644789fafaca62490bf388f76158f8
miinsoo <ms1226f@gmail.com>
2026-04-08 00:23:04 +0900

    feat: notification 도메인 구조 세팅

---
+++ Minsu/umc10th/src/main/java/com/example/umc10th/domain/notification/entity/NotificationType.java
@@ -0,0 +1,4 @@
+package com.example.umc10th.domain.notification.entity;
+
+public class NotificationType {
+}
---
+++ Minsu/umc10th/src/main/java/com/example/umc10th/domain/notification/exception/NotificationException.java
@@ -0,0 +1,4 @@
+package com.example.umc10th.domain.notification.exception;
+
+public class NotificationException extends RuntimeException {
+}
