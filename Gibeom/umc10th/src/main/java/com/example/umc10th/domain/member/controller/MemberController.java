c186b3289022aad61647cfc00d7abea36bb73bec
kite-pp <kiuten558@hansung.ac.kr>
2026-04-28 18:17:54 +0900

    feat: MemberController 구현

--- Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import com.example.umc10th.global.apiPayload.code.MemberSuccessCode;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class MemberController {
+
+    private MemberService memberService;
+    //마이페이지
+    @PostMapping("/v1/users/me")
+    public ApiResponse<MemberResDTO.GetInfo> getInfo(
+            @RequestBody MemberResDTO.GetInfo dto
+    ){
+        BaseSuccessCode code = MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code, memberService.getInfo(dto));
+    }
 }
