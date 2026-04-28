7c38e19230b88d34c390b035a05d9dae4e98ca9c
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-04-28 23:31:16 +0900

    fix: UserController 내에 홈 화면 조회 메소드 속 쿼리 파라미터에 누락되었던 검증을 추가

--- Joonseok/src/main/java/com/umc/study/domain/user/web/controller/UserController.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/web/controller/UserController.java
@@ -2,6 +2,8 @@ package com.umc.study.domain.user.web.controller;
 
 import com.umc.study.domain.user.exception.code.UserSuccessCode;
 import com.umc.study.global.apiPayload.ApiResponse;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +20,8 @@ public class UserController {
     @GetMapping("/home")
     public ResponseEntity<ApiResponse<?>> getMain(
             // TODO JWT에서 유저를 추출해서 주입
-            @RequestParam Integer page,
-            @RequestParam Integer size
+            @RequestParam @Min(message = "page값은 1 이상이어야 합니다.", value = 1) Integer page,
+            @RequestParam @Min(message = "size값은 1 이상이어야 합니다.", value = 1) @Max(message = "size값은 10을 넘길 수 없습니다.", value = 10) Integer size
     ) {
 
         // TODO Service 객체에 실제 비즈니스 로직을 채우고, DI 받은 객체로 호출하여, 응답을 반환받도록 함.
