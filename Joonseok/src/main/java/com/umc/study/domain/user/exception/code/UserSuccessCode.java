ed698da0f199304334d576f60380291e5fb8f43f
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-04-28 23:24:44 +0900

    feat: User 도메인에 종속된 SuccessCode 열거형 객체에 홈화면 조회 성공했을 때의 코드를 추가

--- Joonseok/src/main/java/com/umc/study/domain/user/exception/code/UserSuccessCode.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/exception/code/UserSuccessCode.java
@@ -1,4 +1,19 @@
 package com.umc.study.domain.user.exception.code;
 
-public enum UserSuccessCode {
+import com.umc.study.global.apiPayload.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserSuccessCode implements BaseResponseCode {
+
+    HOME_OK(HttpStatus.OK,
+            "USER200_1",
+            "홈 화면 조회에 성공했습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
