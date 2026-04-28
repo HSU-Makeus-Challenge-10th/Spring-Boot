8f29073b726977e16d175dfe812daeaad4a2b370
kite-pp <kiuten558@hansung.ac.kr>
2026-04-28 18:09:23 +0900

    fest: 정의/비정의 된 예외처리 GeneralExceptionAdvice 추가

---
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -0,0 +1,43 @@
+package com.example.umc10th.global.apiPayload.handler;
+
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.Exception.MemberException;
+import com.example.umc10th.global.apiPayload.Exception.ProjectException;
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.GeneralErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j //Lombok 어노테이션
+@RestControllerAdvice
+public class GeneralExceptionAdvice {
+
+    // 프로젝트에서 발생한 예외 처리
+    @ExceptionHandler(ProjectException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMemberException(
+            ProjectException e
+    ) {
+        BaseErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(ApiResponse.onFailure(errorCode, null));
+    }
+
+    // 그 외의 정의되지 않은 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> handleException(
+            Exception ex
+    ) {
+        log.error("Unhandled Exception occurred", ex);
+        BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(
+                                code,
+                                "서버 내부 오류가 발생했습니다. 관리자에게 문의하세요"
+                        )
+                );
+    }
+
+
+}
