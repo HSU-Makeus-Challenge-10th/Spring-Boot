5ff17ce2ca34f3c2cd3454af9a7d7ca15936b559
kite-pp <kiuten558@hansung.ac.kr>
2026-04-28 21:00:33 +0900

    fix: 클래스 레벨 매핑 경로 수정

--- Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("./api")
+@RequestMapping("/api")
 public class MemberController {
 
     private MemberService memberService;
