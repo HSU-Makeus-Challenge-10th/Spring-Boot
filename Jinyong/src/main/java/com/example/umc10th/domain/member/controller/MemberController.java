aa04963bf2cf2e1bdab8442d5c1ed55bc429d1e9
JinYoung Shin <sjyoungzzang@gmail.com>
2026-04-28 22:38:28 +0900

    feat: 5주차 API 응답 통일 및 에러 핸들러 구현

--- Jinyong/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ Jinyong/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,44 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class MemberController {
+
+    // 1. 회원가입 API
+    @PostMapping("/auth/signup")
+    public ApiResponse<MemberResDTO.JoinResultDTO> join(@RequestBody MemberReqDTO.JoinDTO request) {
+        // 명세서에 맞춘 가짜 데이터 생성
+        MemberResDTO.JoinResultDTO dummy = MemberResDTO.JoinResultDTO.builder()
+                .userId(1L)
+                .nickname(request.getNickname())
+                .email(request.getEmail())
+                .build();
+        return ApiResponse.onSuccess(dummy);
+    }
+
+    // 2. 홈 화면 조회 API
+    @GetMapping("/home")
+    public ApiResponse<MemberResDTO.HomeResultDTO> getHome() {
+        MemberResDTO.MissionDetailDTO dummyMission = MemberResDTO.MissionDetailDTO.builder()
+                .userMissionId(1L)
+                .missionTitle("10,000원 이상 주문 시 500P 적립")
+                .reward(500)
+                .status("IN_PROGRESS")
+                .build();
+
+        MemberResDTO.HomeResultDTO dummyHome = MemberResDTO.HomeResultDTO.builder()
+                .nickname("nickname012")
+                .point(22500)
+                .missions(List.of(dummyMission))
+                .build();
+
+        return ApiResponse.onSuccess(dummyHome);
+    }
 }
--- Jinyong/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ Jinyong/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,25 @@
 package com.example.umc10th.domain.member.dto;
 
+import lombok.Getter;
+import java.util.List;
+
 public class MemberReqDTO {
+
+    @Getter
+    public static class JoinDTO {
+        String name;
+        String gender;
+        String nickname;
+        String email;
+        String password;
+        String birth;
+        String phoneNumber;
+        List<TermsAgreementDTO> termsAgreements;
+    }
+
+    @Getter
+    public static class TermsAgreementDTO {
+        Long termsId;
+        Boolean isAgreed;
+    }
 }
