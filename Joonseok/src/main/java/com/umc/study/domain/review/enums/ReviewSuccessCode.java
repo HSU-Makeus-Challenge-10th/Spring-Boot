77739d9e0489a39ff1c70f10f84781b855ce09f0
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-04-29 12:16:32 +0900

    feat: 리뷰 컨트롤러의 반환 타입인 ResponseEntity에 사용할 성공 코드를 열거형 객체로 관리하기 위해 enums 패키지 작성 및 하위에 ReviewSuccessCode 열거형 클래스 파일 작성

---
+++ Joonseok/src/main/java/com/umc/study/domain/review/enums/ReviewSuccessCode.java
@@ -0,0 +1,21 @@
+package com.umc.study.domain.review.enums;
+
+import com.umc.study.global.apiPayload.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ReviewSuccessCode implements BaseResponseCode {
+
+    REVIEW_CREATED(
+            HttpStatus.CREATED,
+            "REVIEW201_1",
+            "리뷰 작성에 성공했습니다."
+    );
+
+    private final HttpStatus status;
+    private final String message;
+    private final String code;
+}
