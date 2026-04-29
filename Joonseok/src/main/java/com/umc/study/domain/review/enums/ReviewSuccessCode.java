72a72e1303be36b9e90f17c3af4885b68b1ab73d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-04-29 12:25:39 +0900

    fix: 필드 순서가 잘못되어있던 것 수정

--- Joonseok/src/main/java/com/umc/study/domain/review/enums/ReviewSuccessCode.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/enums/ReviewSuccessCode.java
@@ -16,6 +16,6 @@ public enum ReviewSuccessCode implements BaseResponseCode {
     );
 
     private final HttpStatus status;
-    private final String message;
     private final String code;
+    private final String message;
 }
