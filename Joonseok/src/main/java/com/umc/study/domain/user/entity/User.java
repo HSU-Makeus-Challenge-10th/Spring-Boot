c2ab00cef34f3cf620feb9b234c357cd86a73166
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 22:50:22 +0900

    feat: 사용자 엔터티 클래스 작성

--- Joonseok/src/main/java/com/umc/study/domain/user/entity/User.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/entity/User.java
@@ -1,4 +1,39 @@
 package com.umc.study.domain.user.entity;
 
+import com.umc.study.domain.user.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean isMale;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNum;
+
+    @Column(nullable = false)
+    private Integer pointDeposit;
 }
