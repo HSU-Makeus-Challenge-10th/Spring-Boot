f3bbf32848d4306f36c679a4b70beb16f92e189e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-06 01:00:23 +0900

    feat: user의 주소를 관리하기 위해 Locatoin 객체 선언

--- Joonseok/src/main/java/com/umc/study/domain/mission/entity/Location.java
+++ Joonseok/src/main/java/com/umc/study/domain/mission/entity/Location.java
@@ -1,4 +1,21 @@
 package com.umc.study.domain.mission.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Location {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String detailedAddress;
 }
