db19f811ee74f9d5345afcbfd3b4371d54df63c1
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-06 00:10:35 +0900

    feat: 조회 가능한 최대 페이지를 넘겼을 때, FE에게 더 이상 조회할 수 없다고 예외를 던져주기 위해 예외 코드 작성

--- Joonseok/src/main/java/com/umc/study/domain/mission/exception/code/MissionErrorCode.java
+++ Joonseok/src/main/java/com/umc/study/domain/mission/exception/code/MissionErrorCode.java
@@ -1,4 +1,15 @@
 package com.umc.study.domain.mission.exception.code;
 
-public enum MissionErrorCode {
+import com.umc.study.global.apiPayload.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MissionErrorCode implements BaseResponseCode {
+    PAGE_OVERFLOW("PAGE_OVERFLOW_400", 400, "해당 페이지는 리소스의 개수를 초과하여 조회하고 있습니다.");
+
+    private  final String code;
+    private final Integer status;
+    private final String message;
 }
