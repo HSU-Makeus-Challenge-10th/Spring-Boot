65f7d3be8f285fa95f9e18ad5f9334ba626d84ed
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 22:55:19 +0900

    feat: Review 엔터티 객체 작성

--- Joonseok/src/main/java/com/umc/study/domain/review/entity/Review.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/entity/Review.java
@@ -1,4 +1,32 @@
 package com.umc.study.domain.review.entity;
 
-public class Review {
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.umc.study.domain.user.entity.User;
+import com.umc.study.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Integer score;
+
+    private String content;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "review")
+    @JsonManagedReference
+    private List<ReviewComment> comments;
 }
