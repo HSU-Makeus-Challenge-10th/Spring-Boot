531c5a331b2906030a17a4107d8e1e2b10d03e14
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 22:45:24 +0900

    feat: 이용약관 동의 필드를 테이블 객체로 관리

--- Joonseok/src/main/java/com/umc/study/domain/user/entity/Term.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/entity/Term.java
@@ -1,4 +1,21 @@
 package com.umc.study.domain.user.entity;
 
-public enum Term {
+import com.umc.study.domain.user.enums.TermName;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Term {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private TermName termName;
 }
