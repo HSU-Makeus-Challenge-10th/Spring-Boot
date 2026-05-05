51d0861863fd85a3f268a39e9d84b9d9a4912579
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 23:25:54 +0900

    feat: Review를 생성하는 로직을 포함한 ReviewService 객체를 Bean으로 등록, 선언

--- Joonseok/src/main/java/com/umc/study/domain/review/service/ReviewService.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/service/ReviewService.java
@@ -1,7 +1,41 @@
 package com.umc.study.domain.review.service;
 
+import com.umc.study.domain.review.entity.Review;
+import com.umc.study.domain.review.repository.ReviewRepository;
+import com.umc.study.domain.review.web.dto.CreateReviewReq;
+import com.umc.study.domain.user.entity.User;
+import com.umc.study.domain.user.exception.UserNotFoundException;
+import com.umc.study.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    // create Review
+    @Transactional
+    public Void createReview(Long userId, CreateReviewReq request) {
+
+        // found User
+        User found = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        // build Review
+        Review created = Review.builder()
+                .user(found)
+                .score(request.getScore())
+                .content(request.getContent())
+                .build();
+
+        // create Review
+        reviewRepository.save(created);
+
+        return null;
+    }
 }
