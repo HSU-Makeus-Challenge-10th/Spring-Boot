ec619849676aac28b47a685509bc50ee6fd4b95d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 23:21:24 +0900

    feat: UserRepository에 findById 메소드를 Null-safety하게 조회하도록 메소드 오버라이딩

--- Joonseok/src/main/java/com/umc/study/domain/user/repository/UserRepository.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.umc.study.domain.user.repository;
 
-public class UserRepository {
+import com.umc.study.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findById(Long id);
 }
