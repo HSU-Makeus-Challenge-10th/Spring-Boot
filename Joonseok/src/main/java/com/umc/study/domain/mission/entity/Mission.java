3f379b7901c812be068b5f79ff2c94126b727efc
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 23:00:33 +0900

    feat: 미션 엔터티 객체 작성

--- Joonseok/src/main/java/com/umc/study/domain/mission/entity/Mission.java
+++ Joonseok/src/main/java/com/umc/study/domain/mission/entity/Mission.java
@@ -1,4 +1,28 @@
 package com.umc.study.domain.mission.entity;
 
+import com.umc.study.domain.user.entity.User;
+import com.umc.study.domain.user.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Mission {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer minPrice;
+    private Integer point;
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
