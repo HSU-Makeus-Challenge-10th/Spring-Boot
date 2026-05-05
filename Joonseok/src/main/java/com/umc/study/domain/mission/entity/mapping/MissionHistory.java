90c69c80ae023c3edad268aca644fca0796b4a47
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 23:07:04 +0900

    feat: 손님 사용자가 가게 주인 사용자가 게시한 미션을 수락하는 관계 엔터티 객체 작성

--- Joonseok/src/main/java/com/umc/study/domain/mission/entity/mapping/MissionHistory.java
+++ Joonseok/src/main/java/com/umc/study/domain/mission/entity/mapping/MissionHistory.java
@@ -1,4 +1,27 @@
 package com.umc.study.domain.mission.entity.mapping;
 
+import com.umc.study.domain.mission.entity.Mission;
+import com.umc.study.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MissionHistory {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+    private Boolean isCompleted;
 }
