2962ba53749a770bfe3520dc5be7de6331acf507
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 23:14:50 +0900

    feat: ReviewRepository를 JpaRepository를 상속받게끔 수정

--- Joonseok/src/main/java/com/umc/study/domain/review/repository/ReviewRepository.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,8 @@
 package com.umc.study.domain.review.repository;
 
-public class ReviewRepository {
+import com.umc.study.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
 }
