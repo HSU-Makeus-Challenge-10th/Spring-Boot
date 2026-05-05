9377de758dfd63c09518300b9ed15b2bbf4aaf65
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 22:39:40 +0900

    feat: 선호 음식을 저장받기 위해 음식 종류를 열거형 객체로 선언 및 내용 작성

---
+++ Joonseok/src/main/java/com/umc/study/domain/user/enums/FoodName.java
@@ -0,0 +1,18 @@
+package com.umc.study.domain.user.enums;
+
+public enum FoodName {
+
+    KOREAN,
+    JAPANESE,
+    CHINESE,
+    WESTERN,
+    CHICKEN,
+    STREET,
+    GRILLED,
+    BENTO,
+    LATE_MEAL,
+    FAST_FOOD,
+    DESSERT,
+    ASIAN
+
+}
