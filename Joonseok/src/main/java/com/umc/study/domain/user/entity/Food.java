d683f388ded7880a239a6e5b40abec1fd0184f0d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-05 22:40:48 +0900

    feat: 음식 종류를 테이블, 레코드로 관리하기 위해 엔터티 객체로 선언 및 필드 작성

--- Joonseok/src/main/java/com/umc/study/domain/user/entity/Food.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/entity/Food.java
@@ -1,4 +1,21 @@
 package com.umc.study.domain.user.entity;
 
+import com.umc.study.domain.user.enums.FoodName;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Food {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private FoodName name;
 }
