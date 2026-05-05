f7e6ea7a4c89142b535e2bd98251063ed8f4490e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-06 00:29:11 +0900

    feat: userId 기반으로 myPage를 조회하는 메소드 작성, PhoneNum은 미인증일 수 있으므로, null 필드일 경우 빈문자열을 반환하도록 설계

--- Joonseok/src/main/java/com/umc/study/domain/user/service/UserService.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/service/UserService.java
@@ -1,4 +1,28 @@
 package com.umc.study.domain.user.service;
 
+import com.umc.study.domain.user.entity.User;
+import com.umc.study.domain.user.exception.UserNotFoundException;
+import com.umc.study.domain.user.repository.UserRepository;
+import com.umc.study.domain.user.web.dto.GetMyPageRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public GetMyPageRes getMyPage(Long userId) {
+
+        User found = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new GetMyPageRes(
+                found.getName(),
+                found.getEmail(),
+                found.getPhoneNum() == null ? "" : found.getPhoneNum(),
+                found.getPointDeposit()
+        );
+    }
 }
