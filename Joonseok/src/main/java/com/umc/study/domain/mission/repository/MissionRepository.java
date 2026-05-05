64c23470a368c642f48816cb108e853060e14443
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-06 00:07:47 +0900

    feat: 특정 유저가 성공한, 진행중인 미션을 페이징 쿼리하는 메소드를 MissionRepository에 작성

--- Joonseok/src/main/java/com/umc/study/domain/mission/repository/MissionRepository.java
+++ Joonseok/src/main/java/com/umc/study/domain/mission/repository/MissionRepository.java
@@ -1,4 +1,24 @@
 package com.umc.study.domain.mission.repository;
 
-public class MissionRepository {
+import com.umc.study.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    //
+    @Query("""
+select mh.mission
+from MissionHistory mh
+where mh.user.id = :userId
+    and mh.isCompleted = :isCompleted
+""")
+    Page<Mission> findMissionByUserIdAndIsCompleted(
+            @Param("userId") Long userId,
+            @Param("isCompleted") Boolean isCompleted,
+            Pageable pageable
+    );
 }
