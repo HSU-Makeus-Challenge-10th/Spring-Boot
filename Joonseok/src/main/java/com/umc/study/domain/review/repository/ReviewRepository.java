bc56ff7751f0fd1ab48605be54d738c6d1b36d64
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-08 17:14:07 +0900

    feat: ID순 내가 작성한 리뷰 조회, 별점 높은순 내가 작성한 리뷰 조회하는 JPQL을 DTO Constructor Projection 방식으로 작성

--- Joonseok/src/main/java/com/umc/study/domain/review/repository/ReviewRepository.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/repository/ReviewRepository.java
@@ -1,8 +1,50 @@
 package com.umc.study.domain.review.repository;
 
 import com.umc.study.domain.review.entity.Review;
+import com.umc.study.domain.review.web.dto.ReviewDetail;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("""
+select new com.umc.study.domain.review.web.dto.ReviewDetail(
+    rv.id,
+    rv.restaurant.id,
+    rv.user.name,
+    rv.createdAt,
+    rv.score,
+    rv.content
+) from Review rv
+where rv.user.id = :userId
+    and rv.id < :cursor
+order by rv.score desc
+""")
+    List<ReviewDetail> findReviewDetailByUserIdOrderByScore(
+            @Param("userId") Long userId,
+            @Param("cursor") Long cursor,
+            Pageable pageable);
+
+    @Query("""
+    select new com.umc.study.domain.review.web.dto.ReviewDetail(
+        rv.id,
+        rv.restaurant.id,
+        rv.user.name,
+        rv.createdAt,
+        rv.score,
+        rv.content
+    ) from Review rv
+    where rv.user.id = :userId
+        and rv.id < :cursor
+    order by rv.id desc
+    """)
+    List<ReviewDetail> findReviewDetailByUserId(
+            @Param("userId") Long userId,
+            @Param("cursor") Long cursor,
+            Pageable pageable
+    );
 
 }
