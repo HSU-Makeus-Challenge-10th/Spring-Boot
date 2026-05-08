39891a3d1fead3036cd34fdf402cbd28511e02a6
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-08 17:15:34 +0900

    feat: 분리된 커서, 정렬 순서를 input으로 하여 내가 작성한 리뷰를 조회하는 메소드 작성

--- Joonseok/src/main/java/com/umc/study/domain/review/service/ReviewService.java
+++ Joonseok/src/main/java/com/umc/study/domain/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.umc.study.domain.review.service;
 import com.umc.study.domain.mission.entity.Restaurant;
 import com.umc.study.domain.mission.repository.RestaurantRepository;
 import com.umc.study.domain.review.entity.Review;
+import com.umc.study.domain.review.exception.IllegalReviewTypeException;
 import com.umc.study.domain.review.repository.ReviewRepository;
 import com.umc.study.domain.review.web.dto.CreateReviewReq;
 import com.umc.study.domain.review.web.dto.ReviewDetail;
@@ -11,8 +12,12 @@ import com.umc.study.domain.user.exception.UserNotFoundException;
 import com.umc.study.domain.user.repository.UserRepository;
 import com.umc.study.global.apiPayload.cursor.CursorRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -50,9 +55,38 @@ public class ReviewService {
     }
 
     // find all my review by cursor paging
-    public CursorRes<ReviewDetail, Long> getAllMyReviews() {
-        // 1. query
+    public CursorRes<ReviewDetail, Long> getAllMyReviews(Long userId, String[] cursors, String query, int pageSize) {
+
+        PageRequest pageRequest = PageRequest.of(0, pageSize);
+        long idCursor;
+        List<ReviewDetail> found;
+        switch(query.toLowerCase()) {
+            // 1. set Cursor
+            case "id":
+                idCursor = Long.parseLong(cursors[1]);
+
+                found = reviewRepository.findReviewDetailByUserId(
+                        userId,
+                        idCursor,
+                        pageRequest
+                );
+                break;
+            case "score":
+                idCursor = Long.parseLong(cursors[1]);
+
+                found = reviewRepository.findReviewDetailByUserIdOrderByScore(
+                        userId,
+                        idCursor,
+                        pageRequest
+                );
+                break;
+            default:
+                throw new IllegalReviewTypeException();
+        }
+
+        Long nextCursor = found.getLast().getReviewId();
 
         // return
+        return CursorRes.of(nextCursor, pageSize, found);
     }
 }
