5b1e4be46704d7c0b1ba69be0f06890c8c9118bb
JinYoung Shin <sjyoungzzang@gmail.com>
2026-05-10 21:48:57 +0900

    feat : MissionReqDTO 내 CreateMission 레코드 생성

--- Jinyong/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ Jinyong/src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -1,4 +1,13 @@
 package com.example.umc10th.domain.mission.dto;
 
+import java.time.LocalDate;
+
 public class MissionReqDTO {
+
+    // 가게 미션 생성
+    public record CreateMission(
+            String title,
+            LocalDate deadline,
+            Integer reward
+    ) {}
 }
