f3d178d972260bb0974856f144c4d2ee130219f2
JinYoung Shin <sjyoungzzang@gmail.com>
2026-05-10 22:02:57 +0900

    feat : MissionResDTO 내 페이징 응답을 위한 Pagination 레코드 추가

--- Jinyong/src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
+++ Jinyong/src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
@@ -3,9 +3,11 @@ package com.example.umc10th.domain.mission.dto;
 import lombok.Builder;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class MissionResDTO {
 
+    // 가게 내 미션 조회
     @Builder
     public record MissionInfo(
             Long id,
@@ -13,6 +15,13 @@ public class MissionResDTO {
             String title,
             LocalDate deadline,
             Integer reward
-    ) {
-    }
+    ) {}
+
+    // 페이지네이션 툴
+    @Builder
+    public record Pagination<T>(
+            List<T> data,
+            Integer pageNumber,
+            Integer pageSize
+    ){}
 }
