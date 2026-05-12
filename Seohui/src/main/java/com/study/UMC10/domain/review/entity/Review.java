5eac9aaa29714aa3a6763a56205a41d9e858a9bd
Kimsuhhee04 <kimyesol0428@gmail.com>
2026-05-12 11:55:39 +0900

    feat: 내가 생성한 리뷰들 조회하기

--- Seohui/src/main/java/com/study/UMC10/domain/review/entity/Review.java
+++ Seohui/src/main/java/com/study/UMC10/domain/review/entity/Review.java
@@ -3,17 +3,7 @@ package com.study.UMC10.domain.review.entity;
 import com.study.UMC10.domain.store.entity.Store;
 import com.study.UMC10.domain.user.entity.User;
 import com.study.UMC10.global.apiPayload.code.BaseEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -51,4 +41,7 @@ public class Review extends BaseEntity {
     
     @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
     private List<ReviewPhoto> reviewPhotoList = new ArrayList<>();
+
+    @OneToOne(mappedBy = "review", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private OwnerComment ownerComment;
 }
