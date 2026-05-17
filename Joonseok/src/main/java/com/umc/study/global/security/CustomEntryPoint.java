d75b57b9c8b706991c0c390893982a293fb1716e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-17 17:58:03 +0900

    Feat: Forbidden, Unauthorized 응답 래퍼에 @Component 어노테이션을 부착하여 Bean으로 등록하게끔 처리

--- Joonseok/src/main/java/com/umc/study/global/security/CustomEntryPoint.java
+++ Joonseok/src/main/java/com/umc/study/global/security/CustomEntryPoint.java
@@ -10,9 +10,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class CustomEntryPoint implements AuthenticationEntryPoint {
 
--- Joonseok/src/main/java/com/umc/study/global/security/exception/CustomAccessDenied.java
+++ Joonseok/src/main/java/com/umc/study/global/security/exception/CustomAccessDenied.java
@@ -10,9 +10,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class CustomAccessDenied implements AccessDeniedHandler {
 
