3ff9b526474f72e1e94407bf59942d004c89f7ed
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-17 17:49:25 +0900

    Feat: 권한이 부여되지 않아 인가 단계에서 Forbidden 응답을 JSON 템플릿으로 전달하기 위해 AccessDeniedHandler를 구현한 Custom 객체 선언

---
+++ Joonseok/src/main/java/com/umc/study/global/security/exception/CustomAccessDenied.java
@@ -0,0 +1,37 @@
+package com.umc.study.global.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.umc.study.global.apiPayload.ApiResponse;
+import com.umc.study.global.apiPayload.code.BaseResponseCode;
+import com.umc.study.global.apiPayload.code.GeneralErrorCode;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class CustomAccessDenied implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+
+        BaseResponseCode errorCode = GeneralErrorCode.FORBIDDEN;
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(errorCode.getStatus().value());
+
+        ApiResponse<Void> errorResponse = ApiResponse.onFailure(errorCode, null);
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
