4421d200c218347154488678a124d2da4a8f1b45
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-19 12:17:25 +0900

    fix: CustomUserDetailsService를 빈으로 등록하게끔 Component 어노테이션 추가

--- Joonseok/src/main/java/com/umc/study/global/security/service/CustomUserDetailsService.java
+++ Joonseok/src/main/java/com/umc/study/global/security/service/CustomUserDetailsService.java
@@ -9,8 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
