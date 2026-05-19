1d11a2d3781b39b6e68c13bcbc64025f05129b18
Gibeom <kiuten12345@naver.com>
2026-05-19 19:33:50 +0900

    feat : 회원 조회 Boolean 메소드 추가

--- Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.example.umc10th.domain.member.repository;
 import com.example.umc10th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
+    Boolean existsByNickname(String nickname);
 }
