a4c5eb1cbfd60fc7dfa74aad0e141b7bfa578e28
Gibeom <kiuten12345@naver.com>
2026-05-19 19:53:49 +0900

    feat : SecurityConfig 파일 추가

--- Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.umc10th.global.config;
 
+import com.example.umc10th.global.security.CustomAccessDenied;
+import com.example.umc10th.global.security.CustomEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,32 +18,36 @@ public class SecurityConfig {
     private final String[] allowUris = {
       //swagger 허용
       // 자유롭게 이용할 수 있는 주소 (비로그인)
-      "swagger-ui/**",
+      "/swagger-ui/**",
       "/swagger-resources/**",
       "/v3/api-docs/**",
       //로그인
-      "/auth/**",
+      "/api/auth/**",
     };
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // csrf 방어기능 끄기
-                // 위의 허용한 경로로 요청이 들어오면 모두 허용
-                .authorizeHttpRequests(requests -> requests
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->requests
                         .requestMatchers(allowUris).permitAll()
                         .anyRequest().authenticated()
                 )
-                //스프링 시큐리티가 제공하는 기본 로그인 폼을 사용
                 .formLogin(form -> form
-                        .defaultSuccessUrl("/swagger-ui.html", true)
+                        .defaultSuccessUrl("/swagger-ui/index.html", true)
                         .permitAll()
-                //로그아웃 주소로 요청을 보내면 로그아웃 처리를 진행
-                ).logout(logout -> logout
+                )
+                .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout") // 로그아웃 성공 시 로그인 페이지로 리다이렉트
+                        .logoutSuccessUrl("/login?logout")
                         .permitAll()
-                );
+                )
+                //예외 상황 핸들러
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(customAccessDenied())
+                        .authenticationEntryPoint(customEntryPoint())
+                )
+        ;
         return http.build();
     }
 
@@ -49,5 +56,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomAccessDenied customAccessDenied() {
+        return new CustomAccessDenied();
+    }
+
+    @Bean
+    public CustomEntryPoint customEntryPoint() {
+        return new CustomEntryPoint();
     }
 }
