9d4505c1250149f0306bc7fd454c3df68a653a71
Gibeom <kiuten12345@naver.com>
2026-05-22 13:41:15 +0900

    feat : 토큰 필터 등록

--- Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.example.umc10th.global.config;
 
+import com.example.umc10th.global.filter.JwtAuthFilter;
 import com.example.umc10th.global.security.CustomAccessDenied;
 import com.example.umc10th.global.security.CustomEntryPoint;
+import com.example.umc10th.global.service.CustomUserDetailsService;
+import com.example.umc10th.global.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +15,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
     private final String[] allowUris = {
       //swagger 허용
       // 자유롭게 이용할 수 있는 주소 (비로그인)
@@ -29,14 +39,21 @@ public class SecurityConfig {
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                // URI 허용 여부
                 .authorizeHttpRequests(requests ->requests
+                        // public API 허용
                         .requestMatchers(allowUris).permitAll()
+                        // 그 이외의 API는 인증 필요
                         .anyRequest().authenticated()
                 )
+                //폼 로그인
                 .formLogin(form -> form
                         .defaultSuccessUrl("/swagger-ui/index.html", true)
                         .permitAll()
                 )
+                //세션
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
@@ -67,4 +84,10 @@ public class SecurityConfig {
     public CustomEntryPoint customEntryPoint() {
         return new CustomEntryPoint();
     }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter(jwtUtil, customUserDetailsService);
+    }
+
 }
