6c7e3e4cc0638a32248e050efeabdaa10dc5ea88
JinYoung Shin <sjyoungzzang@gmail.com>
2026-05-25 17:35:42 +0900

    feat: 로그인 요청 DTO 추가

--- Jinyong/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ Jinyong/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -6,7 +6,14 @@ public class MemberReqDTO {
             Long id
     ){}
 
+    // 회원가입
     public record SignUp(
+            String email,
+            String password
+    ){}
+
+    // 로그인
+    public record Login(
             String email,
             String password
     ){}
