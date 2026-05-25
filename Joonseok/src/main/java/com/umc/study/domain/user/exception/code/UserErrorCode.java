e4ad0c1ea3104c4db23c62845c0692dbf768cd14
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-25 17:52:04 +0900

    feat: 비밀번호가 올바르지 않을 때 던질 예외 코드 작성

--- Joonseok/src/main/java/com/umc/study/domain/user/exception/code/UserErrorCode.java
+++ Joonseok/src/main/java/com/umc/study/domain/user/exception/code/UserErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements BaseResponseCode {
-    USER_NOT_FOUND("USER_NOT_FOUND_404", HttpStatus.NOT_FOUND, "해당 멤버를 찾는데 실패했습니다.");
+    USER_NOT_FOUND("USER_404_1", HttpStatus.NOT_FOUND, "해당 멤버를 찾는데 실패했습니다."),
+    PASSWORD_INVALID("USER_400_1", HttpStatus.BAD_REQUEST, "비밀번호가 잘못되었습니다.");
 
     private final String code;
     private final HttpStatus status;
