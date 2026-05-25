bcb8101c06336ef03978714443f78f50bd38d1d3
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-25 16:29:11 +0900

    feat: 명시적으로 폼 기반 로그인을 무효화, 세션 무효화, 폼 기반 로그인 인증-인가 필터 이전에 커스텀한 JwtTokenFilter가 작동하도록 설정

--- Joonseok/src/main/java/com/umc/study/global/config/SecurityConfig.java
+++ Joonseok/src/main/java/com/umc/study/global/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.umc.study.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.umc.study.global.jwt.JwtTokenFilter;
 import com.umc.study.global.security.CustomEntryPoint;
 import com.umc.study.global.security.exception.CustomAccessDenied;
+import com.umc.study.global.security.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,15 +14,20 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+    private final JwtTokenFilter jwtTokenFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
     private final String[] allowUris = {
             "/api/swagger-ui/**",
@@ -37,9 +45,11 @@ public class SecurityConfig {
                         .requestMatchers(allowUris).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/api/swagger-ui/index.html", true)
-                        .permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
+
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
