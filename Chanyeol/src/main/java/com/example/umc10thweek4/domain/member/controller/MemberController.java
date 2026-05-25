8fd8afe2924bb8571670a7ee818ad03f44877b5c
chadchan <112686545+coconutcococode@users.noreply.github.com>
2026-05-26 05:44:07 +0900

    feat: 토큰 기반 내 정보 조회 API 추가

--- Chanyeol/src/main/java/com/example/umc10thweek4/domain/member/controller/MemberController.java
+++ Chanyeol/src/main/java/com/example/umc10thweek4/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.umc10thweek4.domain.member.dto.MemberResDTO;
 import com.example.umc10thweek4.domain.member.exception.code.MemberSuccessCode;
 import com.example.umc10thweek4.domain.member.service.MemberService;
 import com.example.umc10thweek4.global.apiPayload.ApiResponse;
+import com.example.umc10thweek4.global.security.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/v1/users/me")
+    public ResponseEntity<ApiResponse<MemberResDTO.GetInfo>> getMyPage() {
+        Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        return ApiResponse.onSuccessResponse(MemberSuccessCode.MY_PAGE_SUCCESS, memberService.getMyPage(currentMemberId));
+    }
 
     @GetMapping("/v1/users/{userId}")
     public ResponseEntity<ApiResponse<MemberResDTO.GetInfo>> getMyPage(@PathVariable Long userId) {
