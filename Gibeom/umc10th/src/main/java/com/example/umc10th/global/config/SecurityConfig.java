2be1a916b982e1445ea53cbd5b52a4acb1542811
Gibeom <kiuten12345@naver.com>
2026-05-26 13:17:35 +0900

    feat : SecurityConfig 수정

--- Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -1,17 +1,18 @@
 package com.example.umc10th.global.config;
 
-import com.example.umc10th.global.filter.JwtAuthFilter;
+import com.example.umc10th.global.security.filter.JwtAuthFilter;
 import com.example.umc10th.global.security.CustomAccessDenied;
 import com.example.umc10th.global.security.CustomEntryPoint;
-import com.example.umc10th.global.service.CustomUserDetailsService;
-import com.example.umc10th.global.util.JwtUtil;
+import com.example.umc10th.global.security.handler.OAuthSuccessHandler;
+import com.example.umc10th.global.security.service.CustomOAuthService;
+import com.example.umc10th.global.security.service.CustomUserDetailsService;
+import com.example.umc10th.global.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +25,7 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomOAuthService customOAuthService;
 
     private final String[] allowUris = {
       //swagger 허용
@@ -33,6 +35,7 @@ public class SecurityConfig {
       "/v3/api-docs/**",
       //로그인
       "/api/auth/**",
+      "/oauth/**",
     };
 
     @Bean
@@ -43,14 +46,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests ->requests
                         // public API 허용
                         .requestMatchers(allowUris).permitAll()
+                        .requestMatchers("/oauth2/authorization/**").permitAll() // 로그인 진입 주소
+                        .requestMatchers("/oauth/callback/kakao").permitAll()     // ⭐️ 카카오 콜백 주소 허용!
                         // 그 이외의 API는 인증 필요
                         .anyRequest().authenticated()
                 )
                 //폼 로그인
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/swagger-ui/index.html", true)
-                        .permitAll()
-                )
+                .formLogin(AbstractHttpConfigurer::disable)
+
                 //세션
                 .sessionManagement(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -63,6 +66,23 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception
                         .accessDeniedHandler(customAccessDenied())
                         .authenticationEntryPoint(customEntryPoint())
+                )
+                //OAuth
+                .oauth2Login(oauth -> oauth
+                        // 인증 엔트리 포인트
+                        .authorizationEndpoint(auth -> auth
+                                .baseUri("/oauth/authorize")
+                        )
+                        // 콜백 주소
+                        .redirectionEndpoint(redirect -> redirect
+                                .baseUri("/oauth/callback/**")
+                        )
+                        //인증 완료 후 정보 활용
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuthService)
+                        )
+                        // 성공 시 JWT 토큰 발행할 핸들러
+                        .successHandler(oAuthSuccessHandler())
                 )
         ;
         return http.build();
@@ -85,9 +105,12 @@ public class SecurityConfig {
         return new CustomEntryPoint();
     }
 
-    @Bean
-    public JwtAuthFilter jwtAuthFilter() {
+    private JwtAuthFilter jwtAuthFilter() {
         return new JwtAuthFilter(jwtUtil, customUserDetailsService);
     }
 
+    @Bean
+    public OAuthSuccessHandler oAuthSuccessHandler() {
+        return new OAuthSuccessHandler(jwtUtil);
+    }
 }
