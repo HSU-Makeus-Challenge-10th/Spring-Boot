6c0524a9a814ab90f5d76f85f62f56e5d5e2727c
Gibeom <kiuten12345@naver.com>
2026-06-13 18:42:00 +0900

    feat : gitActions 설정

---
+++ Gibeom/umc10th/src/main/java/com/example/umc10th/domain/member/enums/SocialType.java
@@ -0,0 +1,10 @@
+package com.example.umc10th.domain.member.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SocialType {
+    KAKAO,
+}
